4c7d5d1db306d97fe19284f714d0b35390d7456d
Yahya Ramzy Akel <89773568+YahyaRamzy@users.noreply.github.com>
2024-05-12 23:38:02 +0300

    Add getUserByEmail to UserManagementService Add updateUser to UserManagementService Add deleteUser to UserManagementService
    Resolves: #NA
    authored-by: Yahya <yahyaramzy@gmail.com>

--- src/main/java/com/kotobi/app/user_managment/service/UserManagmentService.java
+++ src/main/java/com/kotobi/app/user_managment/service/UserManagmentService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -135,6 +136,80 @@ public class UserManagmentService {
         }
         return response;
     }
+
+    public RequestResponse getUserByEmail(String email){
+        RequestResponse response = new RequestResponse();
+        try {
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User Not Found!"));
+            response.setUser(user);
+            response.setStatusCode(200);
+            response.setMessage("User found : " + email);
+
+        }catch (Exception e){
+            response.setStatusCode(400);
+            response.setMessage("User " + email + " Not Found");
+        }
+        return response;
+    }
+
+    public RequestResponse updateUser(UUID uuid , User updatedUser){
+        RequestResponse response = new RequestResponse();
+
+        try{
+            Optional<User> userOptional = userRepository.findById(uuid);
+            if(userOptional.isPresent()){
+
+                User existingUser = userOptional.get();
+                if(updatedUser.getEmail() != null){
+                    existingUser.setEmail(updatedUser.getEmail());
+                }
+                if(updatedUser.getFirst_name() != null){
+                    existingUser.setFirst_name(updatedUser.getFirst_name());
+                }
+                if(updatedUser.getLast_name() != null){
+                    existingUser.setLast_name(updatedUser.getLast_name());
+                }
+                if(updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()){
+                    existingUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+                }
+
+                User savedUser = userRepository.save(existingUser);
+                response.setUser(savedUser);
+                response.setStatusCode(200);
+                response.setMessage("User Updated !");
+
+
+            }else{
+                response.setStatusCode(404);
+                response.setMessage("User " + uuid + " Not Found!");
+            }
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error occurred while updating user: " + e.getMessage());
+        }
+        return response;
+    }
+
+    public RequestResponse deleteUser(UUID uuid){
+        RequestResponse response = new RequestResponse();
+        try {
+            Optional<User> userOptional = userRepository.findById(uuid);
+            if(userOptional.isPresent()){
+                userRepository.deleteById(uuid);
+                response.setStatusCode(200);
+                response.setMessage("User Deleted !");
+            }else{
+                response.setStatusCode(404);
+                response.setMessage("User " + uuid + " Not Found!");
+            }
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Could not Delete User" + e.getMessage());
+        }
+        return response;
+    }
+
 
 
 
