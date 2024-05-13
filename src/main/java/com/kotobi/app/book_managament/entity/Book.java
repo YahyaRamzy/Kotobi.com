109811a42ecc08ad483c3e5a80b36a11f238b982
Yahya Ramzy Akel <89773568+YahyaRamzy@users.noreply.github.com>
2024-05-13 21:54:37 +0300

    Added Book in Entity Added Genre ENUM in Entity
    Resolves: #NA
    authored-by: Yahya <yahyaramzy@gmail.com>

---
+++ src/main/java/com/kotobi/app/book_managament/entity/Book.java
@@ -0,0 +1,32 @@
+package com.kotobi.app.book_managament.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Document
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String author;
+    @Column(unique = true)
+    private int ISBN;
+    private String publisher;
+    private Date publish_date;
+    private Genre genre;
+    private int page_count;
+    private String language;
+    private String cover_image;
+    private String summary;
+
+}
