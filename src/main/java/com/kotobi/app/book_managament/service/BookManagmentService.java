9d8a2ed3a7b694d3839fe1096192e1c2a3481dbf
Yahya Ramzy Akel <89773568+YahyaRamzy@users.noreply.github.com>
2024-05-17 20:09:41 +0300

    Added findAllBooks Function
    Resolves: #NA
    authored-by: Yahya <yahyaramzy@gmail.com>

--- src/main/java/com/kotobi/app/book_managament/service/BookManagmentService.java
+++ src/main/java/com/kotobi/app/book_managament/service/BookManagmentService.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class BookManagmentService {
@@ -30,4 +32,7 @@ public class BookManagmentService {
         }
     }
 
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
