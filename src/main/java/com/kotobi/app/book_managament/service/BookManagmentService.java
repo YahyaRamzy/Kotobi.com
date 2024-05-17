a2c0ac71387ca01257b1551c8ccd35546f95cfba
Yahya Ramzy Akel <89773568+YahyaRamzy@users.noreply.github.com>
2024-05-17 15:46:26 +0300

    Add Book Function
    Resolves: #NA
    authored-by: Yahya <yahyaramzy@gmail.com>

--- src/main/java/com/kotobi/app/book_managament/service/BookManagmentService.java
+++ src/main/java/com/kotobi/app/book_managament/service/BookManagmentService.java
@@ -1,4 +1,24 @@
 package com.kotobi.app.book_managament.service;
 
+import com.kotobi.app.book_managament.entity.Book;
+import com.kotobi.app.book_managament.repository.BookRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class BookManagmentService {
+    @Autowired
+    BookRepository bookRepository;
+
+    public void insertBook(Book book){
+        int bookISBN = book.getISBN();
+        if(bookRepository.findBookByISBN(bookISBN).isPresent()){
+            throw new IllegalStateException("Book already exists : " + bookISBN);
+        }else{
+            bookRepository.insert(book);
+        }
+    }
+
 }
