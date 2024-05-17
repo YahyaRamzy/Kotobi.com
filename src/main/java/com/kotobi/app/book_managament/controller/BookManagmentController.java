1f1a2b6b22355f5add2d5452622c6770b263efab
Yahya Ramzy Akel <89773568+YahyaRamzy@users.noreply.github.com>
2024-05-17 15:46:26 +0300

    Add Book Function
    Resolves: #NA
    authored-by: Yahya <yahyaramzy@gmail.com>

--- src/main/java/com/kotobi/app/book_managament/controller/BookManagmentController.java
+++ src/main/java/com/kotobi/app/book_managament/controller/BookManagmentController.java
@@ -1,4 +1,20 @@
 package com.kotobi.app.book_managament.controller;
 
+import com.kotobi.app.book_managament.entity.Book;
+import com.kotobi.app.book_managament.service.BookManagmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookManagmentController {
+    @Autowired
+    BookManagmentService bookManagmentService;
+
+    @PostMapping("/admin/addBook")
+    public void addBook(@RequestBody Book book){
+        System.out.println(book);
+        bookManagmentService.insertBook(book);
+    }
 }
--- src/main/java/com/kotobi/app/book_managament/repository/BookRepository.java
+++ src/main/java/com/kotobi/app/book_managament/repository/BookRepository.java
@@ -3,9 +3,12 @@ package com.kotobi.app.book_managament.repository;
 import com.kotobi.app.book_managament.entity.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface BookRepository extends MongoRepository<Book, UUID> {
+public interface BookRepository extends MongoRepository<Book, String> {
+
+    Optional<Book> findBookByISBN(int ISBN);
 
 
 
