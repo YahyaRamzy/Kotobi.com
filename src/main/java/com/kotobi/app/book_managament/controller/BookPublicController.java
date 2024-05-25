1e83de7b4f5436377494a11f295e65975cf76878
Yahya Ramzy Akel <89773568+YahyaRamzy@users.noreply.github.com>
2024-05-26 02:06:20 +0300

    Book Managment Resolves: #NA authored-by: Yahya <yahyaramzy@gmail.com>

---
+++ src/main/java/com/kotobi/app/book_managament/controller/BookPublicController.java
@@ -0,0 +1,35 @@
+package com.kotobi.app.book_managament.controller;
+
+import com.kotobi.app.book_managament.entity.Book;
+import com.kotobi.app.book_managament.entity.Genre;
+import com.kotobi.app.book_managament.service.BookManagmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/public/book")
+public class BookPublicController {
+
+    @Autowired
+    BookManagmentService bookManagmentService;
+    @GetMapping({"/"})
+    public List<Book> getAllBooks(){
+        return bookManagmentService.getAllBooks();
+    }
+
+    @GetMapping("/search")
+    public Optional<List<Book>> searchByBookTitleRegex(@RequestParam String title){
+        return bookManagmentService.getBooksByTitleSearchRegex(title);
+    }
+
+    @GetMapping("/search")
+    public Optional<List<Book>> searchByBookGenre(@RequestParam Genre genre){
+        return bookManagmentService.getBooksByGenre(genre);
+    }
+}
--- src/main/java/com/kotobi/app/book_managament/repository/BookRepository.java
+++ src/main/java/com/kotobi/app/book_managament/repository/BookRepository.java
@@ -4,6 +4,7 @@ import com.kotobi.app.book_managament.entity.Book;
 import com.kotobi.app.book_managament.entity.Genre;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,8 +15,9 @@ public interface BookRepository extends MongoRepository<Book, String> {
     void removeBookByISBN(int ISBN);
     Optional<List<Book>> findBooksByTitleRegex(String regex);
     Optional<List<Book>> findBooksByGenre(Genre genre);
-
-
+    Optional<List<Book>> findBooksByAuthorRegex(String author);
+    Optional<List<Book>> findBooksByLanguage(String lang);
+    Optional<List<Book>> findBooksByPublisher(String publisher);
 
 
 }
